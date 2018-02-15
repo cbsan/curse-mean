8dc9cf00941b52b6528ad0a5b367621ab006b47e
Cristian BSan <cbsan.dev@gmail.com>
2018-02-15 09:07:41 -0200

    service curse

---
+++ client/src/app/cursos/cursos.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class CursosService {
+
+  	constructor() { }
+
+
+  	myList = [
+        {
+            name: 'Angular',
+            price: 140,
+            category: 'Front-End'
+        },{
+            name: 'Express',
+            price: 180,
+            category: 'Front-End'
+        }
+    ];
+
+    createItem(newItem) {
+        this.myList.push(newItem);
+    }
+
+    removeItem(item) {
+        var index = this.myList.indexOf(item);
+        this.myList.splice(index, 1);
+    }
+}
